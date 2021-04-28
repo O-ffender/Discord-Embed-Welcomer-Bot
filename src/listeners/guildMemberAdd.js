39d6dd6ab8482cd2a37916937d3b24670e79babb
Offender <81564919+O-ffender@users.noreply.github.com>
2021-04-28 18:32:46 +0100

    Update guildMemberAdd.js

--- src/listeners/guildMemberAdd.js
+++ src/listeners/guildMemberAdd.js
@@ -16,6 +16,8 @@ class guildMemberAddEvent extends Listener {
         .setDescription(`<#812312497587617793> **for roles**\n<#812312266934059008> **for info**\n\n> Talk in <#812310675657195524> to increase your levels and gain roles!\nReach Level 5 for pic perms!`)
         .setFooter(`We are now at ${member.guild.members.cache.size} members`)
         .setThumbnail('https://media.discordapp.net/attachments/697225482370023439/804650122197336084/image3.gif')
+        .setTimeout(function(){ 
+                }, 5000); //time in milliseconds
 
         this.client.channels.cache.get(config.channelID).send(`<@${member.id}>`, embed)
     }
